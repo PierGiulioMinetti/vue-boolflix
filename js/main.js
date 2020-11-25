28fe65ac3799899a54a75a2a68704a8d8b57404f
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-11-25 16:15:30 +0100

    2 function

--- js/main.js
+++ js/main.js
@@ -34,6 +34,35 @@ const app = new Vue({
         })
         this.movieName = '';
       }, //--> end genera()
+
+      filtra(){
+
+        const self = this;
+        /**
+         * axios
+         */
+        axios.get('https://api.themoviedb.org/3/search/tv', {
+          params: {
+            api_key: '7b9720487c48630ca9c8f0d06516ecac',
+            query: this.movieName
+          }
+        }
+        )
+        .then(function (response) {
+          // handle success
+          console.log(response.data.results);
+          self.array = response.data.results;
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        this.movieName = '';
+
+      }, //-->end filtra
+
+        // *****************************
+        // convert votes in stars
       convertStar(vote_average){
         return Math.round(vote_average / 2);
       } //--> end convertStar()
