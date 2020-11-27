67d648d42e8619ede5dbfaeec123751ec04e9fbf
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-11-27 19:31:51 +0100

    Need fixing filter

--- js/main.js
+++ js/main.js
@@ -83,7 +83,73 @@ const app = new Vue({
             "id": 37,
             "name": "Western"
         }
-    ]
+                  ],
+      seriesGenre: [
+        {
+            "id": 10759,
+            "name": "Action & Adventure"
+        },
+        {
+            "id": 16,
+            "name": "Animation"
+        },
+        {
+            "id": 35,
+            "name": "Comedy"
+        },
+        {
+            "id": 80,
+            "name": "Crime"
+        },
+        {
+            "id": 99,
+            "name": "Documentary"
+        },
+        {
+            "id": 18,
+            "name": "Drama"
+        },
+        {
+            "id": 10751,
+            "name": "Family"
+        },
+        {
+            "id": 10762,
+            "name": "Kids"
+        },
+        {
+            "id": 9648,
+            "name": "Mystery"
+        },
+        {
+            "id": 10763,
+            "name": "News"
+        },
+        {
+            "id": 10764,
+            "name": "Reality"
+        },
+        {
+            "id": 10765,
+            "name": "Sci-Fi & Fantasy"
+        },
+        {
+            "id": 10766,
+            "name": "Soap"
+        },
+        {
+            "id": 10767,
+            "name": "Talk"
+        },
+        {
+            "id": 10768,
+            "name": "War & Politics"
+        },
+        {
+            "id": 37,
+            "name": "Western"
+        }
+                  ]
       
     },
     created(){
@@ -175,7 +241,13 @@ const app = new Vue({
       
       // SELECT SERIES
       filtraSerie(){
-
+        const self = this;
+          
+          if (self.arraySeries.length > 0 ){
+            self.arraySeries = self.arraySeries.filter(element=> element.genre_ids.includes(self.SelectSeries))
+          }
+          
+         console.log(self.arraySeries);
       },
 
 
