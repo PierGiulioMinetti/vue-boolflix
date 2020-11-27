eb3a16d7f0210480ed43a52669e56b0a9574ec42
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-11-27 16:45:31 +0100

    Filter not working

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const app = new Vue({
     data: {
       movieName: '',
       arrayMovie: [],
+      selectMovies: '',
+      SelectSeries: '',
       
       
     },
@@ -70,12 +72,53 @@ const app = new Vue({
 
       }, //-->end filtra
 
+      // CALL MOVIES AND SERIES FROM API
       filterMovies(){
         this.genera();
         this.filtra();
       },
 
-        // *****************************
+
+      // // NON FUNZIONA DA QUI
+      // // // SELECT MOVIES
+      // filtraFilm(){
+      //   const self = this;
+      //   /**
+      //    * axios
+      //    */
+      //   axios.get('https://api.themoviedb.org/3/search/movie', {
+      //     params: {
+      //       api_key: '7b9720487c48630ca9c8f0d06516ecac',
+      //       query: this.movieName,
+      //       language: 'it-IT'
+      //     }
+      //   }
+      //   )
+      //   .then(function (response) {
+      //     // handle success
+          
+      //     console.log('lista film' + self.listaFilm);
+      //     if (self.selectMovies !== ''){
+      //       self.arrayMovie = self.arrayMovie.filter(element=> element.genres.id === self.selectMovies)
+      //     }
+      //     console.log(response.data.results);
+      //     self.arrayMovie = self.arrayMovie.concat(self.listaFilm);
+          
+      //   })
+      //   .catch(function (error) {
+      //     // handle error
+      //     console.log(error);
+      //   })
+      //   // this.movieName = '';
+      //   self.arrayMovie= [];
+      // }, //--> end genera()
+      
+      // SELECT SERIES
+      filtraSerie(){
+
+      },
+
+
         // convert votes in stars
       convertStar(vote_average){
         return Math.ceil(vote_average / 2);
@@ -83,5 +126,5 @@ const app = new Vue({
 
 
     } //--> end methods
-
+  
   }) //--> end vue
