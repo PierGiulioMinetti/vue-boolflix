5cd9232c83c4051e1704e3ae728e56b19640639f
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-11-27 17:51:52 +0100

    Movies filter working

--- js/main.js
+++ js/main.js
@@ -5,7 +5,84 @@ const app = new Vue({
       arrayMovie: [],
       selectMovies: '',
       SelectSeries: '',
-      
+      movieGenre : [
+        {
+            "id": 28,
+            "name": "Action"
+        },
+        {
+            "id": 12,
+            "name": "Adventure"
+        },
+        {
+            "id": 16,
+            "name": "Animation"
+        },
+        {
+            "id": 35,
+            "name": "Comedy"
+        },
+        {
+            "id": 80,
+            "name": "Crime"
+        },
+        {
+            "id": 99,
+            "name": "Documentary"
+        },
+        {
+            "id": 18,
+            "name": "Drama"
+        },
+        {
+            "id": 10751,
+            "name": "Family"
+        },
+        {
+            "id": 14,
+            "name": "Fantasy"
+        },
+        {
+            "id": 36,
+            "name": "History"
+        },
+        {
+            "id": 27,
+            "name": "Horror"
+        },
+        {
+            "id": 10402,
+            "name": "Music"
+        },
+        {
+            "id": 9648,
+            "name": "Mystery"
+        },
+        {
+            "id": 10749,
+            "name": "Romance"
+        },
+        {
+            "id": 878,
+            "name": "Science Fiction"
+        },
+        {
+            "id": 10770,
+            "name": "TV Movie"
+        },
+        {
+            "id": 53,
+            "name": "Thriller"
+        },
+        {
+            "id": 10752,
+            "name": "War"
+        },
+        {
+            "id": 37,
+            "name": "Western"
+        }
+    ]
       
     },
     created(){
@@ -79,39 +156,29 @@ const app = new Vue({
       },
 
 
-      // // NON FUNZIONA DA QUI
-      // // // SELECT MOVIES
-      // filtraFilm(){
-      //   const self = this;
-      //   /**
-      //    * axios
-      //    */
-      //   axios.get('https://api.themoviedb.org/3/search/movie', {
-      //     params: {
-      //       api_key: '7b9720487c48630ca9c8f0d06516ecac',
-      //       query: this.movieName,
-      //       language: 'it-IT'
-      //     }
-      //   }
-      //   )
-      //   .then(function (response) {
-      //     // handle success
+      // NON FUNZIONA DA QUI
+      // // SELECT MOVIES
+      filtraFilm(){
+        const self = this;
+        /**
+         * axios
+         */
+        
+      
+          // handle success
+          // self.arrayMovie= [];
+
           
-      //     console.log('lista film' + self.listaFilm);
-      //     if (self.selectMovies !== ''){
-      //       self.arrayMovie = self.arrayMovie.filter(element=> element.genres.id === self.selectMovies)
-      //     }
-      //     console.log(response.data.results);
-      //     self.arrayMovie = self.arrayMovie.concat(self.listaFilm);
+          if (self.arrayMovie.length > 0 ){
+            self.arrayMovie = self.arrayMovie.filter(element=> element.genre_ids.includes(self.selectMovies))
+          }
           
-      //   })
-      //   .catch(function (error) {
-      //     // handle error
-      //     console.log(error);
-      //   })
-      //   // this.movieName = '';
-      //   self.arrayMovie= [];
-      // }, //--> end genera()
+         console.log(self.arrayMovie);
+          
+         
+        
+        // this.movieName = '';
+      }, //--> end genera()
       
       // SELECT SERIES
       filtraSerie(){
